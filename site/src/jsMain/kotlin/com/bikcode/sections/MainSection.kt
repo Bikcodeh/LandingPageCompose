f64bf3f18e372eb62248aa2a68d35b613fff903b
Bikcode <bikcodeh@gmail.com>
2023-06-01 16:38:59 -0500

    create constants file and create partially main section

---
+++ site/src/jsMain/kotlin/com/bikcode/sections/MainSection.kt
@@ -0,0 +1,30 @@
+package com.bikcode.sections
+
+import androidx.compose.runtime.Composable
+import com.bikcode.util.Constants.SECTION_WIDTH
+import com.bikcode.util.Res
+import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.objectFit
+import com.varabyte.kobweb.silk.components.graphics.Image
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun MainSection() {
+    Box(modifier = Modifier.maxWidth(SECTION_WIDTH), contentAlignment = Alignment.TopCenter) {
+        MainBackground()
+    }
+}
+
+@Composable
+fun MainBackground() {
+    Image(
+        modifier = Modifier.fillMaxSize().objectFit(ObjectFit.Cover),
+        src = Res.Image.background,
+        desc = "Background"
+    )
+}
---
+++ site/src/jsMain/kotlin/com/bikcode/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.bikcode.util
+
+import org.jetbrains.compose.web.css.px
+
+object Constants {
+    val SECTION_WIDTH = 1920.px
+}
+
+object Res {
+    object Image {
+        const val background = "background.svg"
+    }
+}
