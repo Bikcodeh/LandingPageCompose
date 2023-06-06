f43c09b7a7e45d1a832347da7bb1d529eb78b371
Bikcode <bikcodeh@gmail.com>
2023-06-06 17:25:15 -0500

    animate links and logo with ComponentStyle

--- site/src/jsMain/kotlin/com/bikcode/components/Header.kt
+++ site/src/jsMain/kotlin/com/bikcode/components/Header.kt
@@ -3,6 +3,8 @@ package com.bikcode.components
 import androidx.compose.runtime.Composable
 import com.bikcode.models.Section
 import com.bikcode.models.Theme
+import com.bikcode.styles.LogoStyle
+import com.bikcode.styles.NavigationItemStyle
 import com.bikcode.util.Constants.FONT_FAMILY
 import com.bikcode.util.Res
 import com.varabyte.kobweb.compose.css.FontWeight
@@ -16,6 +18,7 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.toModifier
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 
@@ -35,6 +38,7 @@ fun Header() {
 fun LeftSide() {
     Row {
         Image(
+            modifier = LogoStyle.toModifier(),
             src = Res.Image.logo,
             desc = "Logo image"
         )
@@ -53,7 +57,7 @@ fun RightSide() {
     ) {
         Section.values().take(6).forEach { section ->
             Link(
-                modifier = Modifier
+                modifier = NavigationItemStyle.toModifier()
                     .fontFamily(FONT_FAMILY)
                     .fontSize(18.px)
                     .fontWeight(FontWeight.Normal)
