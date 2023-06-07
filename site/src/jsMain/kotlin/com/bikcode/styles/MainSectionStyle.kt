291842859ca45b488d4aff8a83cc4e547237b84a
Bikcode <bikcodeh@gmail.com>
2023-06-06 20:19:13 -0500

    main section with text and image with social bar

--- site/src/jsMain/kotlin/com/bikcode/styles/MainSectionStyle.kt
+++ site/src/jsMain/kotlin/com/bikcode/styles/MainSectionStyle.kt
@@ -6,12 +6,14 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.transform
 import com.varabyte.kobweb.compose.ui.modifiers.transition
+import com.varabyte.kobweb.compose.ui.modifiers.width
 import com.varabyte.kobweb.silk.components.style.ComponentStyle
 import com.varabyte.kobweb.silk.components.style.anyLink
 import com.varabyte.kobweb.silk.components.style.hover
 import org.jetbrains.compose.web.ExperimentalComposeWebApi
 import org.jetbrains.compose.web.css.deg
 import org.jetbrains.compose.web.css.ms
+import org.jetbrains.compose.web.css.px
 
 val NavigationItemStyle by ComponentStyle {
     base {
@@ -52,5 +54,16 @@ val SocialLinkStyle by ComponentStyle {
     }
     hover {
         Modifier.color(Theme.Primary.rgb)
+    }
+}
+
+val MainButtonStyle by ComponentStyle {
+    base {
+        Modifier
+            .width(100.px)
+            .transition(CSSTransition(property = "width", duration = 200.ms))
+    }
+    hover {
+        Modifier.width(120.px)
     }
 }
--- site/src/jsMain/kotlin/com/bikcode/util/Constants.kt
+++ site/src/jsMain/kotlin/com/bikcode/util/Constants.kt
@@ -5,11 +5,18 @@ import org.jetbrains.compose.web.css.px
 object Constants {
     const val FONT_FAMILY = "Verdana"
     val SECTION_WIDTH = 1920.px
+    const val LOREM_IPSUM_LONG =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    const val LOREM_IPSUM_SHORTEST =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    const val LOREM_IPSUM_SHORT =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 }
 
 object Res {
     object Image {
         const val background = "background.svg"
         const val logo = "logo.svg"
+        const val mainImage = "main_image.png"
     }
 }
